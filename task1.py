0437f7b4744b3c303e2b687e743090183f60a1e7
Mansi1901 <43661828+Mansi1901@users.noreply.github.com>
2018-10-02 16:32:51 -0300

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-#open file
-
-myfile = open("running-config.cfg")
-
-#Running throughout the file and collect interfacenames
-
-for line in myfile:
-    #first remove whitespace and extra stuff
-    line = line.strip()
